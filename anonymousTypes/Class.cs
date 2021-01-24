42c84c84779a02aec4518761c4fc7bdd658a4d09
candandogan <candandogan@github.com>
2021-01-24 13:25:20 +0300

    Add project files.

---
+++ anonymousTypes/Class.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anonymousTypes
+{
+    class Class
+    {
+        //student list created
+        private List<student> students = new List<student>();
+
+        //method of adding students
+        public void Add(student std)
+        {
+
+            bool isStudentExist = false;
+            string addQuestion;
+            foreach (var student in students)
+            {
+                if (student.Name == std.Name && student.Surname == std.Surname) //Checking whether the student to be added is in the classroom
+                {
+                    isStudentExist = true;
+                }
+            }
+            if (isStudentExist) 
+            {
+                Console.WriteLine("This student already recorded to class. Do you still want to add it to class? (yes/no)");
+                addQuestion = Console.ReadLine().ToLower();
+                if (addQuestion == "yes")
+                    students.Add(std);
+                else
+                    Console.WriteLine("Student not added to class.");
+            }
+            else
+            {
+                students.Add(std);
+            }
+        }
+        //method of searching students by name
+        public List<student> GetStudentsByName(string name)
+        {
+            return students.Where(s => s.Name.Contains(name)).ToList();
+        }
+
+        //method of searching students by id
+        public student GetStudentById(int id)
+        {
+            return students.FirstOrDefault(s => s.Id == id);
+        }
+
+        //remove students method
+        public List<student> RemoveStudentById(int id)
+        {
+            var toRemove = students.SingleOrDefault(s => s.Id == id);
+            if (toRemove != null)
+                students.Remove(toRemove);
+            return students;
+        }
+
+        //The method of listing students enrolled in the class
+        public void showStudents()
+        {
+            foreach (var item in students)
+            {
+                Console.WriteLine($"Students name: {item.Name}, surname:{item.Surname},  id:{item.Id}");
+            }
+        }
+    }
+}
---
+++ anonymousTypes/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anonymousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var classroom = new Class();
+
+            try
+            {
+                while (true)
+                {
+                   
+                    Console.WriteLine("WHICH OPERATION DO YOU WANT TO DO? \n1-Add Student \n2-Find Student by Name \n3-Find Student by Id \n4-Remove Student \n5-Show Students List \n6-Exit");
+                    int a = Convert.ToInt32(Console.ReadLine());
+
+                    if (a == 1)
+                    {
+                        Console.WriteLine("Enter the name of the student you want to add: ");
+                        string name = Console.ReadLine();
+
+                        Console.WriteLine("Enter the surname of the student you want to add: ");
+                        string surname = Console.ReadLine();
+
+                        Console.WriteLine("Enter the id of the student you want to add: ");
+                        int b = Convert.ToInt32(Console.ReadLine());
+
+                        classroom.Add(new student { Name = name, Surname = surname, Id = b });
+
+                        Console.WriteLine("Your registration has been added!\n\n\n");
+
+                    }
+                    if (a == 2)
+                    {
+                        Console.WriteLine("Enter the name of the student you want to search: ");
+                        string arama = Console.ReadLine();
+                        var f = classroom.GetStudentsByName(arama);
+                        f.ForEach(stud => Console.WriteLine($"{stud.Name} named student's id is: {stud.Id} .\n\n\n"));
+                    }
+                    if (a == 3)
+                    {
+                        Console.WriteLine("Enter the id of the student you want to search: ");
+                        int c = Convert.ToInt32(Console.ReadLine());
+                        var getId = classroom.GetStudentById(c);
+                        Console.WriteLine(getId.Id + "id student's name is: " + getId.Name+ "\n\n\n");
+                    }
+                    if (a == 4)
+                    {
+                        Console.WriteLine("Enter the school number of the student you want to delete: ");
+                        int d = Convert.ToInt32(Console.ReadLine());
+                        classroom.RemoveStudentById(d);
+                        Console.WriteLine("Student Registration Removed!\n\n\n");
+                        
+                    }
+                    if (a == 5)
+                    {
+                        Console.WriteLine("LIST OF STUDENTS ENROLLED IN THE CLASS");
+                        classroom.showStudents();
+                    }
+                    if (a == 6)
+                    {
+                        break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occured: " + e);
+            }
+            Console.ReadLine();
+        }
+    }
+}
